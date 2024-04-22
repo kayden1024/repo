00a5433cd1577fb75f932227059308db2964d2d6
kayden1024 <166461340+kayden1024@users.noreply.github.com>
2024-04-22 16:13:40 +1000

    Add files via upload

---
+++ JS_Greetings.js
@@ -0,0 +1,5 @@
+// Prompt the user to enter their name
+var userName = prompt("Please enter your name:");
+
+// Display a greeting with the user's name
+alert("Hello, " + userName + "! Welcome to our website.");
